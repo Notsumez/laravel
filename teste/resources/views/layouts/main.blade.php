61fd31ef89bf4ac3bd6afc28ec4b61fd3ce8b04a
Andrey da Hora <adahorapirola@gmail.com>
2023-06-09 11:00:54 -0300

    Layouts com Blade

--- teste/resources/views/layouts/main.blade.php
+++ teste/resources/views/layouts/main.blade.php
@@ -6,12 +6,12 @@
     <!-- Ao digitar "/" o laravel identifica como a pasta public -->
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/script.js"></script>
-    <title>Laravel Teste</title>
+    <title>@yield('title')</title>
 </head>
 <body>
-    <h1 class="red">Teste</h1>
-    <h4>{{ $test }}</h4>
-    <br>
-    <img src="/images/imagem1.jpg" alt="imagem 1" width="1000px">
+    @yield('content')
+    <footer>
+        <p>Andrey da Hora &copy; 2023</p>
+    </footer>
 </body>
 </html>
